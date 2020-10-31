2cffc60106c88eb94286d603ada20c899d56c66b
moritz <fink.moritz@gmail.com>
2020-10-31 22:35:59 +0100

    Switch currency from € to $

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -3,6 +3,6 @@ import 'package:intl/intl.dart';
 class Util {
 
   static String formatPrize(double prize) {
-    return NumberFormat("#,##0.00", "en_US").format(prize) + ' €';
+    return NumberFormat("#,##0.00", "en_US").format(prize) + ' \$';
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,11 @@ void main() {
     final store = Store<AppState>(AppState.reduce, initialState: AppState());
     await tester.pumpWidget(FlutterReduxDemoApp(store));
 
-    expect(find.text('Total: 0.00 €'), findsOneWidget);
+    expect(find.text('Total: 0.00 \$'), findsOneWidget);
 
     await tester.tap(find.byIcon(Icons.add_shopping_cart).first);
     await tester.pump();
 
-    expect(find.text('Total: 0.00 €'), findsNothing);
+    expect(find.text('Total: 0.00 \$'), findsNothing);
   });
 }
